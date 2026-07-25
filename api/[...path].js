faa587734ae6e768e0dc9b00de7af95fd2db5f4c
iuukai <790331286@163.com>
2026-07-26 05:29:47 +0800

    a12

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,10 @@
 const app = require('../apps/express-api/src/app')
 
+const PREFIX = '/_site/'
+
 module.exports = (req, res) => {
-  app(req, res)
+	if (req.url.startsWith(PREFIX) || req.url === '/_site') {
+		req.url = req.url.slice(PREFIX.length - 1) || '/'
+	}
+	app(req, res)
 }
