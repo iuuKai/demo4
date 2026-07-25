9c08cbe9011ba6686d5d43087b06032b42817e4a
iuukai <790331286@163.com>
2026-07-26 05:04:32 +0800

    a8

--- api/[...path].js
+++ api/[...path].js
@@ -1,9 +1,13 @@
 const app = require('../apps/express-api/src/app')
 
-module.exports = (req, res) => {
-	const prefix = '/api/_express'
-	if (req.url.startsWith(prefix) || req.url === '/api/_express/') {
-		req.url = req.url.slice(prefix.length) || '/'
+const PREFIX = '/api/_express'
+
+function handler(req, res) {
+	if (req.url.startsWith(PREFIX)) {
+		req.url = req.url.slice(PREFIX.length) || '/'
 	}
 	app(req, res)
 }
+
+module.exports = handler
+module.exports.handle = handler
